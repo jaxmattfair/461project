ab3bdd49719796047cb96915737a68517e4a551c
Arnav Naval <arnav.naval@gmail.com>
2024-09-07 20:47:56 -0400

    commander added

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,3 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log("Test to see if npm start works");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
+import commander from "commander";
+
 console.log("Test to see if npm start works");
