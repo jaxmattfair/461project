a33495994a8c2e859ed789aac3071a2201eb7aac
Akanksh Rao <akankshrao@Akankshs-MacBook-Pro.local>
2024-09-08 18:20:58 -0400

    Pushing it with json

--- dist/cli.js
+++ dist/cli.js
@@ -1,10 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.program = void 0;
 const commander_1 = require("commander");
 //Create new command program
 const program = new commander_1.Command();
+exports.program = program;
 //Set CLI details
 program
     .name('run')
     .description('description for this CLI for project phase 1')
     .version('1.0.0');
+program.parse(process.argv);
